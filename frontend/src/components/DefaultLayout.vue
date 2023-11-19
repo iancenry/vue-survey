0aa3854a2ddc10fe1cb88f8e412f1c76d3178622
Ian Cenry <iancenry@gmail.com>
2023-11-19 18:49:37 +0300

    chore: layout refractor

--- frontend/src/components/DefaultLayout.vue
+++ frontend/src/components/DefaultLayout.vue
@@ -49,7 +49,7 @@
                   <MenuItems
                     class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                   >
-                    <MenuItem v-slot="{ active }">
+                    <MenuItem>
                       <a
                         @click="signout"
                         class="block px-4 py-2 text-sm text-gray-700 cursor-pointer"
--- frontend/src/views/SurveyView.vue
+++ frontend/src/views/SurveyView.vue
@@ -1,10 +1,7 @@
 <template>
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">My Survey</div>
-  </main>
+  <PageComponent title="Surveys">Slot is like children in react</PageComponent>
 </template>
+
+<script setup>
+import PageComponent from '../components/PageComponent.vue'
+</script>
